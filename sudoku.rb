243d9b1f5263c97824f3bce5f78c6a472838df15
Peter Saxton <peterhsaxton@gmail.com>
2014-02-05 13:46:34 +0000

    Im lost

--- sudoku.rb
+++ sudoku.rb
@@ -33,7 +33,7 @@ end
 get '/' do
   sudoku = random_sudoku
   session[:solution] = sudoku
-  @current_solution = puzzle sudoku.dup
+  @current_solution = session[:current_solution].chars.map { |e| e == 0 ? "" : e.to_s } || puzzle(sudoku.dup)
   erb :index
 end
 
